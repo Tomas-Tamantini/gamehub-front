829ce3c28f767062febd3df6f14850f2624e7148
Tomas Tamantini <tomasbft@gmail.com>
2025-02-18 20:06:26 -0300

    Implement login dialog

--- src/app/core/components/login-dialog/login-dialog.component.spec.ts
+++ src/app/core/components/login-dialog/login-dialog.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginDialogComponent } from './login-dialog.component';
+import { MatDialogRef } from '@angular/material/dialog';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 describe('LoginDialogComponent', () => {
   let component: LoginDialogComponent;
   let fixture: ComponentFixture<LoginDialogComponent>;
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<LoginDialogComponent>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginDialogComponent]
+      imports: [LoginDialogComponent],
+      providers: [
+        provideAnimationsAsync(),
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) }],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoginDialogComponent);
     component = fixture.componentInstance;
+    dialogRefSpy = TestBed.inject(MatDialogRef) as jasmine.SpyObj<MatDialogRef<LoginDialogComponent>>
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onCancel', () => {
+    beforeEach(() => {
+      component.onCancel();
+    });
+
+    it('should close the dialog', () => {
+      expect(dialogRefSpy.close).toHaveBeenCalled();
+    });
+
+    it('should not return playerId', () => {
+      expect(dialogRefSpy.close).not.toHaveBeenCalledWith(component.playerId);
+    });
+  });
+
+  describe('onSubmit', () => {
+    beforeEach(() => {
+      component.playerId.set('Alice');
+      component.onSubmit();
+    });
+
+    it('should close the dialog', () => {
+      expect(dialogRefSpy.close).toHaveBeenCalled();
+    });
+
+    it('should return playerId', () => {
+      expect(dialogRefSpy.close).toHaveBeenCalledWith("Alice");
+    });
   });
 });
--- src/app/core/components/login-dialog/login-dialog.component.ts
+++ src/app/core/components/login-dialog/login-dialog.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import {
+  MatDialogRef,
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-login-dialog',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+  ],
   templateUrl: './login-dialog.component.html',
   styleUrl: './login-dialog.component.scss'
 })
 export class LoginDialogComponent {
+  private readonly dialogRef = inject(MatDialogRef<LoginDialogComponent>);
+  readonly playerId = model<string>();
 
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    this.dialogRef.close(this.playerId());
+  }
 }
