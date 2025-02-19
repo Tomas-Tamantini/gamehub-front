926a4bd48fbee368582b5bed5f2a4cbf5239a18a
Tomas Tamantini <tomasbft@gmail.com>
2025-02-19 09:29:32 -0300

    Start implementing rooms list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./core/components/header/header.component";
+import { RoomsListComponent } from "./features/components/rooms-list/rooms-list.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, RoomsListComponent]
 })
 export class AppComponent {
 }
