8de5acc631552745da58907f33490190137c59c6
Tomas Tamantini <tomasbft@gmail.com>
2025-02-19 14:17:50 -0300

    Redirect to game room

--- src/app/features/components/room-card/room-card.component.spec.ts
+++ src/app/features/components/room-card/room-card.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RoomCardComponent } from './room-card.component';
 import { ComponentRef } from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
 
 describe('RoomCardComponent', () => {
   let component: RoomCardComponent;
   let componentRef: ComponentRef<RoomCardComponent>;
   let fixture: ComponentFixture<RoomCardComponent>;
+  let router: Router;
+  let navigateSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RoomCardComponent]
+      imports: [RoomCardComponent],
+      providers: [provideRouter([])]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(RoomCardComponent);
+    router = TestBed.inject(Router);
+    navigateSpy = spyOn(router, 'navigate');
     component = fixture.componentInstance;
     componentRef = fixture.componentRef;
     componentRef.setInput('room', { roomId: 123, playerIds: [] });
@@ -36,4 +41,20 @@ describe('RoomCardComponent', () => {
     fixture.detectChanges();
     expect(component.canJoin()).toBeFalse();
   });
+
+  describe('join game', () => {
+    beforeEach(() => {
+      component.joinRoom();
+    });
+
+    it('should redirect to game page', () => {
+      const expectedRoute = ['/room', 123];
+      expect(navigateSpy).toHaveBeenCalledWith(expectedRoute, jasmine.any(Object));
+    });
+
+    it('should redirect with action "join"', () => {
+      const expectedParams = { queryParams: { action: 'join' } };
+      expect(navigateSpy).toHaveBeenCalledWith(jasmine.any(Array), expectedParams);
+    });
+  })
 });
--- src/app/features/components/room-card/room-card.component.ts
+++ src/app/features/components/room-card/room-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { RoomSummary } from '../../models/room-summary.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-card',
@@ -11,12 +12,13 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class RoomCardComponent {
   room = input.required<RoomSummary>();
+  private readonly router = inject(Router);
 
   canJoin() {
     return !this.room().isFull;
   }
 
   joinRoom() {
-    throw new Error('Not implemented');
+    this.router.navigate(['/room', this.room().roomId], { queryParams: { action: 'join' } });
   }
 }
