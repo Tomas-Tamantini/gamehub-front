71aedc33b27e7ce9645c86c7ae3841126ef33165
Tomas Tamantini <tomasbft@gmail.com>
2025-02-19 14:31:50 -0300

    Initialize game room component

---
+++ src/app/features/components/game-room/game-room.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-room',
+  imports: [],
+  templateUrl: './game-room.component.html',
+  styleUrl: './game-room.component.scss'
+})
+export class GameRoomComponent {
+
+}
