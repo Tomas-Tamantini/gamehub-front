653f6d8fcb98e5939d1f3b27bc47143f338ba6fa
Tomas Tamantini <tomasbft@gmail.com>
2025-02-19 23:21:22 -0300

    Create skeleton content for loading state

---
+++ src/app/core/components/cards-skeleton/cards-skeleton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-skeleton',
+  imports: [],
+  templateUrl: './cards-skeleton.component.html',
+  styleUrl: './cards-skeleton.component.scss'
+})
+export class CardsSkeletonComponent {
+
+}
--- src/app/features/components/rooms-list/rooms-list.component.ts
+++ src/app/features/components/rooms-list/rooms-list.component.ts
@@ -2,21 +2,30 @@ import { Component, inject, model, OnInit } from '@angular/core';
 import { RoomSummary } from './room-summary.model';
 import { RoomCardComponent } from './room-card/room-card.component';
 import { HttpService } from '../../../core/services/http.service';
+import { CardsSkeletonComponent } from "../../../core/components/cards-skeleton/cards-skeleton.component";
 
 @Component({
   selector: 'app-rooms-list',
-  imports: [RoomCardComponent],
+  imports: [RoomCardComponent, CardsSkeletonComponent],
   templateUrl: './rooms-list.component.html',
   styleUrl: './rooms-list.component.scss'
 })
 export class RoomsListComponent implements OnInit {
   rooms = model<RoomSummary[]>([]);
+  isLoading = model<boolean>(true);
   private readonly httpService = inject(HttpService);
 
   ngOnInit() {
+    this.isLoading.set(true);
     this.httpService.getGameRooms().subscribe({
-      next: rooms => this.rooms.set(rooms.items),
-      error: error => console.error('Failed to fetch game rooms', error)
+      next: rooms => {
+        this.rooms.set(rooms.items);
+        this.isLoading.set(false);
+      },
+      error: error => {
+        console.error('Failed to fetch game rooms', error);
+        this.isLoading.set(false);
+      }
     });
   }
 }
