7013a0e80c3a3b081f32f4f6f0f0012acebbec2e
Tomas Tamantini <tomasbft@gmail.com>
2025-02-19 22:28:02 -0300

    Define list response type

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { RoomSummary } from '../../features/components/rooms-list/room-summary.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { ListResponse } from '../models/list-response';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,8 @@ import { environment } from '../../../environments/environment';
 export class HttpService {
   private http = inject(HttpClient);
 
-  public getGameRooms(): Observable<RoomSummary[]> {
+  public getGameRooms(): Observable<ListResponse<RoomSummary>> {
     const queryParams = new URLSearchParams({ game_type: 'chinese_poker' });
-    return this.http.get<RoomSummary[]>(`${environment.apiUrl}/rooms?${queryParams.toString()}`);
+    return this.http.get<ListResponse<RoomSummary>>(`${environment.apiUrl}/rooms/?${queryParams.toString()}`);
   }
 }
