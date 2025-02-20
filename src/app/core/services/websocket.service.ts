27d9b4556010ad30606e1d372a76f49beb98726f
Tomas Tamantini <tomasbft@gmail.com>
2025-02-20 10:50:48 -0300

    Start defining socket service

---
+++ src/app/core/services/websocket.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  connect() {
+    throw new Error('Method not implemented.');
+  }
+
+  disconnect() {
+    throw new Error('Method not implemented.');
+  }
+}
