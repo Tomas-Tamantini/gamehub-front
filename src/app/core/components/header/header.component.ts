12721d5d3e749e9ff1f8c808b4096f55ec4ec446
Tomas Tamantini <tomasbft@gmail.com>
2025-02-21 14:48:54 -0300

    Remove obsolete import

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,11 +3,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { AccountMenuComponent } from "../account-menu/account-menu.component";
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, AccountMenuComponent, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, AccountMenuComponent, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
