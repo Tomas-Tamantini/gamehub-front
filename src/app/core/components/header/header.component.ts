205962b5342c2be9e752e061f3ea4c8e181c4040
Tomas Tamantini <tomasbft@gmail.com>
2025-02-21 13:34:52 -0300

    Navigate to home page when clicking title

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,10 +3,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { AccountMenuComponent } from "../account-menu/account-menu.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, AccountMenuComponent],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, AccountMenuComponent, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
