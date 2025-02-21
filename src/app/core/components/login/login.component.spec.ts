662a486f8c5773db10b574b732119fd529ab5acb
Tomas Tamantini <tomasbft@gmail.com>
2025-02-21 12:32:12 -0300

    Implement login page

--- src/app/core/components/login/login.component.spec.ts
+++ src/app/core/components/login/login.component.spec.ts
@@ -1,23 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let navigateSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent],
+      providers: [
+        provideAnimationsAsync(),
+        { provide: AuthService, useValue: jasmine.createSpyObj('AuthService', ['login']) },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
+    const router = TestBed.inject(Router);
+    navigateSpy = spyOn(router, 'navigate');
+    authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not allow user to login if player id is empty', () => {
+    component.playerId.set(" ");
+    expect(component.canSubmit()).toBeFalse();
+  });
+
+  it('should allow user to login if player id is not empty', () => {
+    component.playerId.set("Alice");
+    expect(component.canSubmit()).toBeTrue();
+  });
+
+  describe('onSubmit', () => {
+    beforeEach(() => {
+      component.playerId.set(" Alice ");
+      component.onSubmit();
+    });
+
+
+    it('should trim playerId before submitting', () => {
+      expect(authServiceSpy.login).toHaveBeenCalledWith("Alice");
+    });
+
+    it('should redirect to home page after successful login', () => {
+      expect(navigateSpy).toHaveBeenCalledWith(['/']);
+    });
   });
 });
--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  playerId = model<string>("");
 
+  canSubmit(): boolean {
+    return this.playerId().trim().length > 0;
+  }
+
+  onSubmit(): void {
+    this.authService.login(this.playerId().trim());
+    this.router.navigate(['/']);
+  }
 }
