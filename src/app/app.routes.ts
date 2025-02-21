ec8b60a87b14f65af2e1d33a94ea0b9d80c4e3e4
Tomas Tamantini <tomasbft@gmail.com>
2025-02-21 11:55:45 -0300

    Start implementing login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RoomsListComponent } from './features/components/rooms-list/rooms-list.component';
 import { GameRoomComponent } from './features/components/game-room/game-room.component';
 import { isLoggedInGuard } from './core/guards/is-logged-in.guard';
+import { LoginComponent } from './core/components/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
     { path: 'rooms', component: RoomsListComponent, canActivate: [isLoggedInGuard] },
     { path: 'room/:id', component: GameRoomComponent, canActivate: [isLoggedInGuard] }
 ];
