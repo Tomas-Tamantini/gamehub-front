0ab068d3e04f07ccf3b1cc090aab354d6ad262e8
Tomas Tamantini <tomasbft@gmail.com>
2025-02-23 13:06:07 -0300

    Create hand component

---
+++ src/app/features/components/game-room/game/player/hand/hand.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hand',
+  imports: [],
+  templateUrl: './hand.component.html',
+  styleUrl: './hand.component.scss'
+})
+export class HandComponent {
+
+}
--- src/app/features/components/game-room/game/player/player.component.ts
+++ src/app/features/components/game-room/game/player/player.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Player } from './player.model';
 import { MoneyPipe } from '../../../../../core/pipes/money.pipe';
+import { HandComponent } from "./hand/hand.component";
 
 @Component({
   selector: 'app-player',
-  imports: [MoneyPipe],
+  imports: [MoneyPipe, HandComponent],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss'
 })
