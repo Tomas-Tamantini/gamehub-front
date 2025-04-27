8f3f4379b1b49f70d6fa5ff08b9592393d993e02
Tomas Tamantini <tomasbft@gmail.com>
2025-04-27 14:21:58 -0300

    Format with prettier

--- src/app/core/models/message.model.ts
+++ src/app/core/models/message.model.ts
@@ -1,38 +1,41 @@
-import { PrivateView } from "./private-view.model";
-import { RoomSummary } from "./room-summary.model";
-import { SharedGameState } from "./shared-view.model";
-
+import { PrivateView } from './private-view.model';
+import { RoomSummary } from './room-summary.model';
+import { SharedGameState } from './shared-view.model';
 
 export interface GameState {
-    roomId: number;
-    sharedView: SharedGameState | null;
-    privateView: PrivateView | null;
+  roomId: number;
+  sharedView: SharedGameState | null;
+  privateView: PrivateView | null;
 }
 
 export interface ErrorMessage {
-    messageType: "ERROR";
-    payload: { error: string };
+  messageType: 'ERROR';
+  payload: { error: string };
 }
 
 export interface TurnTimer {
-    roomId: number;
-    playerId: string;
-    secondsRemaining: number;
+  roomId: number;
+  playerId: string;
+  secondsRemaining: number;
 }
 
 export interface GameRoomUpdateMessage {
-    messageType: "GAME_ROOM_UPDATE";
-    payload: RoomSummary;
+  messageType: 'GAME_ROOM_UPDATE';
+  payload: RoomSummary;
 }
 
 export interface GameStateMessage {
-    messageType: "GAME_STATE";
-    payload: GameState;
+  messageType: 'GAME_STATE';
+  payload: GameState;
 }
 
 export interface TurnTimerAlertMessage {
-    messageType: "TURN_TIMER_ALERT";
-    payload: TurnTimer;
+  messageType: 'TURN_TIMER_ALERT';
+  payload: TurnTimer;
 }
 
-export type Message = ErrorMessage | GameRoomUpdateMessage | GameStateMessage | TurnTimerAlertMessage;
+export type Message =
+  | ErrorMessage
+  | GameRoomUpdateMessage
+  | GameStateMessage
+  | TurnTimerAlertMessage;
