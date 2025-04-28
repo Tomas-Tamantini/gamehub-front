fdeaabd3f2edd501170cd8a2ff1aebe16dad2a91
Tomas Tamantini <tomasbft@gmail.com>
2025-04-28 11:49:59 -0300

    Indicate bot moves with a badge

--- src/app/features/components/game-room/game/player/hand-history/hand-history.component.ts
+++ src/app/features/components/game-room/game/player/hand-history/hand-history.component.ts
@@ -1,17 +1,18 @@
 import { Component, input } from '@angular/core';
 import { Hand } from '../player.model';
 import { SuitPipe } from '../../../../../../core/pipes/suit.pipe';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-hand-history',
-  imports: [SuitPipe],
+  imports: [SuitPipe, MatBadgeModule],
   templateUrl: './hand-history.component.html',
-  styleUrl: './hand-history.component.scss'
+  styleUrl: './hand-history.component.scss',
 })
 export class HandHistoryComponent {
   history = input.required<Hand[]>();
 
   handsToDisplay(): Hand[] {
-    return this.history().slice(-2)
+    return this.history().slice(-2);
   }
 }
